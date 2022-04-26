b570d2e5e393d1aa895465352748a7cebe6dbcaa
Rahul Nanwani <50020924+rahul-nanwani@users.noreply.github.com>
2022-04-26 20:19:10 +0530

    it's done for good

--- assets/index.js
+++ assets/index.js
@@ -1,31 +1,31 @@
 
-var start = new Date("May 24, 2021 23:18:00").getTime();
-var x = setInterval(function () {
-    var now = new Date().getTime();
-    var elapsed = now - start;
-
-    var days = Math.floor(elapsed / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((elapsed % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
-
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hrs").innerHTML = hours;
-    document.getElementById("mins").innerHTML = minutes;
-    document.getElementById("secs").innerHTML = seconds;
-}, 1000);
-
 // var start = new Date("May 24, 2021 23:18:00").getTime();
-// var end = new Date("Apr 18, 2022 19:57:00").getTime();
+// var x = setInterval(function () {
+//     var now = new Date().getTime();
+//     var elapsed = now - start;
 
-// var elapsed = end - start;
+//     var days = Math.floor(elapsed / (1000 * 60 * 60 * 24));
+//     var hours = Math.floor((elapsed % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     var minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
+//     var seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
 
-// var days = Math.floor(elapsed / (1000 * 60 * 60 * 24));
-// var hours = Math.floor((elapsed % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-// var minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
-// var seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
+//     document.getElementById("days").innerHTML = days;
+//     document.getElementById("hrs").innerHTML = hours;
+//     document.getElementById("mins").innerHTML = minutes;
+//     document.getElementById("secs").innerHTML = seconds;
+// }, 1000);
 
-// document.getElementById("days").innerHTML = days;
-// document.getElementById("hrs").innerHTML = hours;
-// document.getElementById("mins").innerHTML = minutes;
-// document.getElementById("secs").innerHTML = seconds;
+var start = new Date("May 24, 2021 23:18:00").getTime();
+var end = new Date("Apr 25, 2022 22:08:00").getTime();
+
+var elapsed = end - start;
+
+var days = Math.floor(elapsed / (1000 * 60 * 60 * 24));
+var hours = Math.floor((elapsed % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
+
+document.getElementById("days").innerHTML = days;
+document.getElementById("hrs").innerHTML = hours;
+document.getElementById("mins").innerHTML = minutes;
+document.getElementById("secs").innerHTML = seconds;
